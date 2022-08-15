4364b1c502be7f2398a4e9f224543549b273e3cb
Praveen Suluvai <praveen.suluvai@immutable.com>
2022-08-15 13:14:38 +1000

    DX-1052: Usage example showing how to add server

--- examples/publicapi/list_assets/main.go
+++ examples/publicapi/list_assets/main.go
@@ -11,6 +11,10 @@ import (
 	"immutable.com/imx-core-sdk-golang/config"
 )
 
+const (
+	AlchemyAPIKey = "Provide your alchemy API Key"
+)
+
 func PrettyStruct(data interface{}) (string, error) {
 	val, err := json.MarshalIndent(data, "", "    ")
 	if err != nil {
@@ -23,10 +27,33 @@ func main() {
 
 	configuration := api.NewConfiguration()
 	configuration.Debug = true // Enables debug logging.
-	apiClient := api.NewAPIClient(configuration)
+
+	// Multiple ways to configure the server
+	// 1. Set default host on config
+	// 2. Per request config via context.
+
+	/*
+		// Setup default host
+		configuration.Host = "api.x.immutable.com"
+		configuration.Scheme = "https"
+		ctx := context.Background()
+	*/
+
+	// or
+
+	/*
+		// Alternatively add to configuration servers then it can be selectable with context value as below.
+		mainnetConfig := config.GetConfig(config.MainNet, AlchemyAPIKey)
+		configuration.Servers = append(configuration.Servers, api.ServerConfiguration{
+			URL:         mainnetConfig.CoreAPIEndpoint,
+			Description: "Production Server",
+		})
+	*/
 
 	// Using context value to switch/specify the server before sending request. If nothig is specified default server will be used which will be first one in the open api spec list.
 	ctx := context.WithValue(context.Background(), api.ContextServerIndex, config.Ropsten)
+
+	apiClient := api.NewAPIClient(configuration)
 	listAssetsResponse, r, err := apiClient.AssetsApi.ListAssets(ctx).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `AssetsApi.ListAssets``: %v\n", err)
@@ -42,5 +69,4 @@ func main() {
 			fmt.Println(res)
 		}
 	}
-
 }
