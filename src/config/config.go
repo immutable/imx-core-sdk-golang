e731246540c36e2933888f72fd7d1de118ab2a0b
Praveen Suluvai <Praveen.suluvai@immutable.com>
2022-08-15 16:17:34 +1000

    Dx 1052 config (#41)
    * DX-1052: Initial auto generated code from openapi-generator
    
    * DX-1052: Make file updates
    
    * DX-1052: Updated config to as per openapi-generator
    
    * DX-1052: Fixed repo name.
    
    * DX-1052: Removed client template file not needed anymore
    
    * DX-1052: No need for client factory fucntion
    
    * DX-1052: Usage example showing how to add server

--- src/config/config.go
+++ src/config/config.go
@@ -1,59 +1,45 @@
 package config
 
-import (
-	"net/url"
-
-	"immutable.com/imx-core-sdk-golang/api/client"
-)
-
 type Config struct {
 	EthereumClientEndpoint      string
-	CoreAPIEndpoint             url.URL
+	CoreAPIEndpoint             string
 	RegistrationContractAddress string
 	StarkContractAddress        string
 }
 
-type Network string
+type Network int
 
+// The order of this enum should match with the order specified in the OpenApi Spec document.
 const (
-	Dev     = Network("dev")
-	Ropsten = Network("ropsten")
-	MainNet = Network("mainnet")
+	// Dev     Network = iota // Dev testing network
+	Ropsten = iota // Staging network
+	MainNet        // Production network
 )
 
 func getBaseConfigs() map[Network]Config {
 	return map[Network]Config{
-		Dev: {
-			EthereumClientEndpoint: "https://eth-goerli.g.alchemy.com/v2/",
-			CoreAPIEndpoint: url.URL{
-				Scheme: "https",
-				Host:   "api.dev.x.immutable.com",
-			},
-			RegistrationContractAddress: "0x2F76E4e48A5f9e517765B70a4DEc67781d35A199",
-			StarkContractAddress:        "0x3e6e01355bB66925a65D372bf9c9f3835d9964fA",
-		},
+		// Dev: {
+		// 	EthereumClientEndpoint:      "https://eth-goerli.g.alchemy.com/v2/",
+		// 	CoreAPIEndpoint:             "https://api.dev.x.immutable.com",
+		// 	RegistrationContractAddress: "0x2F76E4e48A5f9e517765B70a4DEc67781d35A199",
+		// 	StarkContractAddress:        "0x3e6e01355bB66925a65D372bf9c9f3835d9964fA",
+		// },
 		Ropsten: {
-			EthereumClientEndpoint: "https://eth-ropsten.alchemyapi.io/v2/",
-			CoreAPIEndpoint: url.URL{
-				Scheme: "https",
-				Host:   "api.ropsten.x.immutable.com",
-			},
+			EthereumClientEndpoint:      "https://eth-ropsten.alchemyapi.io/v2/",
+			CoreAPIEndpoint:             "https://api.ropsten.x.immutable.com",
 			RegistrationContractAddress: "0x6C21EC8DE44AE44D0992ec3e2d9f1aBb6207D864",
 			StarkContractAddress:        "0x4527BE8f31E2ebFbEF4fCADDb5a17447B27d2aef",
 		},
 		MainNet: {
-			EthereumClientEndpoint: "https://eth-mainnet.alchemyapi.io/v2/",
-			CoreAPIEndpoint: url.URL{
-				Scheme: "https",
-				Host:   "api.x.immutable.com",
-			},
+			EthereumClientEndpoint:      "https://eth-mainnet.alchemyapi.io/v2/",
+			CoreAPIEndpoint:             "https://api.x.immutable.com",
 			RegistrationContractAddress: "0x72a06bf2a1CE5e39cBA06c0CAb824960B587d64c",
 			StarkContractAddress:        "0x5FDCCA53617f4d2b9134B29090C87D01058e27e9",
 		},
 	}
 }
 
-func GetAPIURL(network Network) url.URL {
+func GetAPIURL(network Network) string {
 	return getBaseConfigs()[network].CoreAPIEndpoint
 }
 
@@ -62,12 +48,4 @@ func GetConfig(network Network, alchemyKey string) Config {
 	baseConfig.EthereumClientEndpoint += alchemyKey
 
 	return baseConfig
-}
-
-func NewTransportConfig(coreEndPoint *url.URL) *client.TransportConfig {
-	return &client.TransportConfig{
-		Host:     coreEndPoint.Host,
-		BasePath: coreEndPoint.Path,
-		Schemes:  []string{coreEndPoint.Scheme},
-	}
 }
