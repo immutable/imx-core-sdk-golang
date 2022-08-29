5a5488cbc492e97e81508cffaca80fd9f84cfe1e
Aung T. T. Kyaw <zatlite@gmail.com>
2022-08-29 10:57:21 +1000

    chore: :rename variable as per review comment

--- examples/publicapi/create_project/main.go
+++ examples/publicapi/create_project/main.go
@@ -14,11 +14,11 @@ import (
 )
 
 func main() {
-	environment := config.GetConfig(config.Sandbox, "alchemy api key")
+	sandboxEnv := config.GetConfig(config.Sandbox, "alchemy api key")
 	configuration := api.NewConfiguration()
 	apiClient := api.NewAPIClient(configuration)
 	ctx := context.WithValue(context.Background(), api.ContextServerIndex, config.Sandbox)
-	l1signer, err := utils.NewBaseL1Signer("Project Owner Private Key", environment.ChainID)
+	l1signer, err := utils.NewBaseL1Signer("Project Owner Private Key", sandboxEnv.ChainID)
 	if err != nil {
 		log.Panicf("error in creating BaseL1Signer: %v\n", err)
 	}
