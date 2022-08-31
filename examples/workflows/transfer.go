7afda417f41351980ebe05407cd4104f66ebf790
Aung T. T. Kyaw <zatlite@gmail.com>
2022-08-31 15:39:24 +1000

    feat: implement CreateBatchNftTransfer
    namespace clash on Client.CreateTransfer resolved by specifying Client.TransferApi.CreateTransfer

--- examples/workflows/transfer.go
+++ examples/workflows/transfer.go
@@ -35,3 +35,45 @@ func demoTransferWorkflow(ctx context.Context, c *immutable.Client, l1signer imm
 	log.Printf("Running %s completed.", getCurrentFunctionName())
 	log.Println("-------------------------------------------------------")
 }
+
+func demoBatchNftTransferWorkflow(ctx context.Context, c *immutable.Client, l1signer immutable.L1Signer, l2signer immutable.L2Signer) {
+	log.Println("-------------------------------------------------------")
+	log.Printf("Running %s", getCurrentFunctionName())
+
+	// To declare tokens, use tokens.NewSignableTokenERC721 method.
+	// For more information about ETH, ERC20, and ERC721 tokens see https://docs.x.immutable.com/docs/token-data-object
+	signableToken1 := immutable.SignableERC721Token("Token ID Here", "Token Address Hex Here")
+	signableToken2 := immutable.SignableERC721Token("Token ID Here", "Token Address Hex Here")
+	amount := "1"
+	sender := l1signer.GetAddress()
+	receiver := "Receiver Address Here"
+
+	transferRequest1 := api.SignableTransferDetails{
+		Amount:   amount,
+		Receiver: receiver,
+		Token:    signableToken1,
+	}
+	transferRequest2 := api.SignableTransferDetails{
+		Amount:   amount,
+		Receiver: receiver,
+		Token:    signableToken2,
+	}
+
+	batchTransferRequest := api.GetSignableTransferRequest{
+		SenderEtherKey: sender,
+		SignableRequests: []api.SignableTransferDetails{
+			transferRequest1,
+			transferRequest2,
+		},
+	}
+
+	response, err := c.CreateBatchNftTransfer(ctx, l1signer, l2signer, batchTransferRequest)
+	if err != nil {
+		log.Panicf("error calling batch transfer workflow: %v", err)
+	}
+	val, _ := json.MarshalIndent(response, "", "  ")
+	log.Printf("response:\n%s\n", val)
+
+	log.Printf("Running %s completed.", getCurrentFunctionName())
+	log.Println("-------------------------------------------------------")
+}
--- transfer.go
+++ transfer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/immutable/imx-core-sdk-golang/api"
 )
 
@@ -43,4 +44,64 @@ func (c *Client) CreateTransfer(
 		return nil, fmt.Errorf("error when calling `TransfersApi.CreateTransferV1`: %v, HTTP response body: %v", err, httpResponse.Body)
 	}
 	return response, nil
+}
+
+// CreateBatchNftTransfer performs a bulk transfer of NFTs given an array of models.SignableToken and their receivers.
+func (c *Client) CreateBatchNftTransfer(
+	ctx context.Context,
+	l1signer L1Signer,
+	l2signer L2Signer,
+	request api.GetSignableTransferRequest,
+) (*api.CreateTransferResponse, error) {
+	data, httpResponse, err := c.GetSignableTransfer(ctx).GetSignableTransferRequestV2(request).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("error when calling `TransfersApi.GetSignableTransfer`: %v, HTTP response body: %v", err, httpResponse.Body)
+	}
+
+	ethSignatureBytes, err := l1signer.SignMessage(data.SignableMessage)
+	if err != nil {
+		return nil, fmt.Errorf("error generating EthSignature from SignableMessage: %v", err)
+	}
+
+	ethSignature := hexutil.Encode(ethSignatureBytes)
+	ethAddress := l1signer.GetAddress()
+
+	transferRequests, err := getSignedTransferRequests(data.SignableResponses, l2signer)
+	if err != nil {
+		return nil, err
+	}
+
+	response, httpResponse, err := c.TransfersApi.CreateTransfer(ctx).
+		CreateTransferRequestV2(api.CreateTransferRequest{
+			Requests:       transferRequests,
+			SenderStarkKey: data.SenderStarkKey,
+		}).
+		XImxEthAddress(ethAddress).
+		XImxEthSignature(ethSignature).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("error when calling `TransfersApi.CreateTransfer`: %v, HTTP response body: %v", err, httpResponse.Body)
+	}
+	return response, nil
+}
+
+// getSignedTransferRequests iterates through signableTransfers, signs each payloadHash and returns an array of pointers to models.TransferRequest.
+func getSignedTransferRequests(signableTransfers []api.SignableTransferResponseDetails, l2signer L2Signer) ([]api.TransferRequest, error) {
+	mapped := make([]api.TransferRequest, len(signableTransfers))
+	for i, transfer := range signableTransfers {
+		starkSignature, err := l2signer.SignMessage(transfer.PayloadHash)
+		if err != nil {
+			return nil, fmt.Errorf("error generating StarkSignature from PayloadHash %s for AssetID %s: %v", transfer.PayloadHash, transfer.AssetId, err)
+		}
+		mapped[i] = api.TransferRequest{
+			Amount:              transfer.Amount,
+			AssetId:             transfer.AssetId,
+			ExpirationTimestamp: transfer.ExpirationTimestamp,
+			Nonce:               transfer.Nonce,
+			ReceiverStarkKey:    transfer.ReceiverStarkKey,
+			ReceiverVaultId:     transfer.ReceiverVaultId,
+			SenderVaultId:       transfer.SenderVaultId,
+			StarkSignature:      starkSignature,
+		}
+	}
+	return mapped, nil
 }
