0c6e5b28bf68a7e78ef4ab6dd3aee0eb6cb952a6
Praveen Suluvai <praveen.suluvai@immutable.com>
2022-11-14 12:26:48 +1000

    Release version 0.2.2

--- imx/api/configuration.go
+++ imx/api/configuration.go
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.2.1/go",
+		UserAgent:        "OpenAPI-Generator/0.2.2/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
---
+++ tasks.md
@@ -0,0 +1,7 @@
+Denomination - Done
+Method documentation - Done
+Package Documentation
+Testing all examples
+Auto test examples
+Update Readme
+Check about NewMethods with Alex.
