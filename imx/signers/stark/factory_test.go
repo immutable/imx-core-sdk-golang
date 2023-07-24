0cfae8e4f6189e29a284f37da7bf76e6bf660e3b
Carl Menezes <carl.menezes@immutable.com>
2023-07-24 13:20:30 +1200

    Fix lint issues

--- imx/signers/stark/factory_test.go
+++ imx/signers/stark/factory_test.go
@@ -76,42 +76,42 @@ func TestStarkSignerFactory_GenerateLegacyKey_GrindkeyFix(t *testing.T) {
 			wantPublicKey: "0x0579f97e8084dfbbead9bffd750df780e06d8c09a3ba7f40ebe51d46b47df043",
 		},
 		{
-			name:          "grindKeyfix hashed once", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix hashed once",
 			privateKey:    "0x1a245f2fa7c4f04a65d45a3877ad00b1423d081490dcc1a7050c8d7c11ec5c8f",
 			wantPublicKey: "0x07ca61905954bdd858ae63704b111da5ca52b30a21fa9aaeee9aef9b24e89607",
 		},
 		{
-			name:          "grindKeyfix hashed more than once", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix hashed more than once",
 			privateKey:    "0xe516a5b715dc53ba4bf06ed98cbe50897921f45f76d4138f8a7d4ba02a89e10d",
 			wantPublicKey: "0x0157c6efcb2c69604c1b46ffeb568b3b10ea7093da762e1daf66b59d9b63b5f9",
 		},
 		{
-			name:          "grindKeyfix is backwards compatible", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix is backwards compatible",
 			privateKey:    "ba3c969f4957e6bf24e5cf8a931bdba4f90d27c01bb7dff738e4593142826db7",
 			wantPublicKey: "0x04eb684f7318d2b90ef8703e6cd77e362414f4997934ee3c99ee50db3411dfef",
 		},
 		{
-			name:          "grindKeyfix hashed once is backwards compatible", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix hashed once is backwards compatible",
 			privateKey:    "0xa25022c521d884d195c119b705ac515fabb48971d3cf2369f188c96b0e6404ef",
 			wantPublicKey: "0x03aa9edf4a5b33f623d3dd37a6df62e5590f172fd4d1a9483532464aa13bcbb9",
 		},
 		{
-			name:          "grindKeyfix hashed more than once is backwards compatible", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix hashed more than once is backwards compatible",
 			privateKey:    "0x719b531dfdbf5e327646ccd992923c9d8846d60767b49d0589d631c1f54d1f12",
 			wantPublicKey: "0x057a8278637277ebaaa63b000185507eb847eec8f69c7758fc10a490d0226e7d",
 		},
 		{
-			name:          "grindKeyfix hashed once generates correct legacy key", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix hashed once generates correct legacy key",
 			privateKey:    "0xe7ecb8f91175446248a6cfa45a9526c85bc4cd7cbd9427e3e65a82d3f5fb8cdc",
 			wantPublicKey: "0x05f8f00b03e896cb8c02a936bab73623fc3651fddabfe0d28fbc309c63642c10",
 		},
 		{
-			name:          "grindKeyfix hashed more than once generates correct legacy key", //from imx-sdk-js, starkCurve.test.ts
+			name:          "grindKeyfix hashed more than once generates correct legacy key",
 			privateKey:    "0x82930dfa052b198828f70529148b02178b03515e910217dab32bf9fc046bc9d9",
 			wantPublicKey: "0x04d34c9f519b2a3538b02c4e939188b591065eb8c2f210277c6ed1de85b16fab",
 		},
 		{
-			name:          "random eth private key with no index generates correct legacy key", //from imx-sdk-js, starkCurve.test.ts
+			name:          "random eth private key with no index generates correct legacy key",
 			privateKey:    "0x0ac63e2143a49a14a87d865c8d7993806ff16ac1c3288ff97101569881f0d306",
 			wantPublicKey: "0x06e6ac4bb44f3295b881532452f90eccb5601314fafe306db17684b47aa388bd",
 		},
