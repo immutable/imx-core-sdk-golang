16dad8065d19c1bf0d1486359566628fc9bd07a2
Kai Hirota <34954529+kaihirota@users.noreply.github.com>
2023-09-04 16:47:03 +1000

    replace alchemy endpoint

--- imx/interface.go
+++ imx/interface.go
@@ -12,10 +12,10 @@ import (
 var (
 	Sandbox = Environment{
 		BaseAPIPath:                 "https://api.sandbox.x.immutable.com",
-		EthereumRPC:                 "https://eth-goerli.alchemyapi.io/v2/",
+		EthereumRPC:                 "https://eth-sepolia.g.alchemy.com/v2/",
 		RegistrationContractAddress: "0x1C97Ada273C9A52253f463042f29117090Cd7D83",
 		CoreContractAddress:         "0x7917eDb51ecD6CdB3F9854c3cc593F33de10c623",
-		ChainID:                     big.NewInt(5), // Goërli
+		ChainID:                     big.NewInt(11155111), // Goërli
 	}
 	Mainnet = Environment{
 		BaseAPIPath:                 "https://api.x.immutable.com",
